373fe2c9be792ea029fcaa18e28278f7fd46b29c
Ahmed Gaber <ahmedgaber20@gmail.com>
2016-03-25 22:45:17 +0200

    remove tabs

--- state.cpp
+++ state.cpp
@@ -1,41 +1,37 @@
 #include "state.h"
 #include <iostream>
+
 state::state() {
 
 }
 
 void state::connect(char x, state* dest) {
-
-//	cout << "from connect : " << x << " " << dest->id << endl;
-	next.insert(make_pair(x, dest));
-	multimap<char, state*>::iterator it = next.begin();
-//	cout << next.size() << endl;
-//	for (it; it != next.end(); it++) {
-//		cout << it->first << " " << it->second->id << endl;
-//	}
+  next.insert(make_pair(x, dest));
+  multimap<char, state*>::iterator it = next.begin();
 }
 
 void state::set_acceptence(bool is_acceptence) {
-	this->is_acceptence = is_acceptence;
-	if (is_acceptence == false) {
-		this->token_type = "";
-	}
+  this->is_acceptence = is_acceptence;
+  if (is_acceptence == false) {
+    this->token_type = "";
+  }
 }
+
 bool state::get_acceptence() {
-	return is_acceptence;
+  return is_acceptence;
 }
 
 void state::set_token_type(string tk) {
-	this->token_type = tk;
-	if (tk == "") {
-		this->is_acceptence = false;
-	}
+  this->token_type = tk;
+  if (tk == "") {
+    this->is_acceptence = false;
+  }
 }
+
 string state::get_token_type() {
-	return token_type;
+  return token_type;
 }
 
 multimap<char, state*> state::get_neighbors() {
-	return next;
-
+  return next;
 }
--- state.h
+++ state.h
@@ -4,21 +4,21 @@
 using namespace std;
 
 class state {
-public :
-	state();
-	int id ;
-	void connect (char x , state* dest) ; // connecting this node to dest node
-	void set_acceptence(bool is_acceptence);
-	bool get_acceptence ();
-	multimap<char,state*> get_neighbors ();
-	void set_token_type(string tk);
-	string get_token_type() ;
 
-	// this -- x --> dest ,
+public:
+  state();
+  int id ;
+  void connect (char x , state* dest) ; // connects the current node to "dest" node
+  void set_acceptence(bool is_acceptence);
+  bool get_acceptence ();
+  multimap<char,state*> get_neighbors ();
+  void set_token_type(string tk);
+  string get_token_type();
+
 private:
-	bool is_acceptence = false;
-	multimap<char, state*> next;
-	string token_type = "" ; // if it is acceptence state , what is accepts , keyword , id ,or what
-	                         // make sure that this parameter is empty string when is_acceptence = false :D
+  bool is_acceptence = false;
+  multimap<char, state*> next;
+  string token_type = "" ; // if it is acceptence state , what is accepts , keyword , id ,or what
+                      // make sure that this parameter is empty string when is_acceptence = false.
 };
 
