9e43de5a83bb6e3f55c4e2bd121f9f1e5a57f25e
Ahmed Gaber <ahmedgaber20@gmail.com>
2016-03-25 22:32:38 +0200

    Add state class

---
+++ state.h
@@ -0,0 +1,24 @@
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+class state {
+public :
+	state();
+	int id ;
+	void connect (char x , state* dest) ; // connecting this node to dest node
+	void set_acceptence(bool is_acceptence);
+	bool get_acceptence ();
+	multimap<char,state*> get_neighbors ();
+	void set_token_type(string tk);
+	string get_token_type() ;
+
+	// this -- x --> dest ,
+private:
+	bool is_acceptence = false;
+	multimap<char, state*> next;
+	string token_type = "" ; // if it is acceptence state , what is accepts , keyword , id ,or what
+	                         // make sure that this parameter is empty string when is_acceptence = false :D
+};
+
